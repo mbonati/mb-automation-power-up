fe149a6bf21f90c8443129feaa1ff8d088f74365
Marco Bonati <marco.bonati@fabrick.com>
2020-07-03 16:11:28 +0200

    MInor

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,8 @@
 var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
+console.log('Loading mb-automation-power-up Connector...');
+
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
 };
